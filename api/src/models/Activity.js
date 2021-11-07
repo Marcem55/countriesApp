aa5833db5de19a5bc350aceb316dd4b72657d2a4
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2021-11-07 15:27:30 -0300

    first commit

---
+++ api/src/models/Activity.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+module.exports= (sequelize) => {
+    sequelize.define('activity', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        difficulty: {
+            type: DataTypes.INTEGER,
+            validate: {
+                min: 1,
+                max: 5
+            }
+        },
+        duration: {
+            type: DataTypes.INTEGER
+        },
+        season: {
+            type: DataTypes.ENUM('Spring', 'Summer', 'Autumn', 'Winter')
+        }
+    });
+};
