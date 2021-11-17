015a78c948fa468adce2e38e649527d7011b72a4
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2021-11-17 16:12:16 -0300

    searchByName ready

--- client/src/components/Search&FilterBar/SearchAndFilterBar.jsx
+++ client/src/components/Search&FilterBar/SearchAndFilterBar.jsx
@@ -1,27 +1,43 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { orderByName, filterActivity, filterContinent } from '../../redux/actions';
+import { orderByName, filterActivity, filterContinent, orderByPopulation, getCountriesByName } from '../../redux/actions';
 import './SearchAndFilterBar.css';
 
 const SearchAndFilterBar = () => {
     const dispatch = useDispatch();
 
     const activities = useSelector(state => state.activities);
+    const [country, setCountry] = useState('');
+
+
+    const handleChange = (e) => {
+        setCountry(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(getCountriesByName(country));
+        setCountry('');
+    }
 
     const handleFilters = (e) => {
         dispatch(filterContinent(e.target.value));
         // dispatch(filterActivity(e.target.value));
+        console.log();
     }
 
     const handleOrders = (e) => {
         dispatch(orderByName(e.target.value));
+        dispatch(orderByPopulation(e.target.value));
     }
 
     return (
         <div className='searchBarContainer'>
-            <div className='searchContainer'>
-                <label className='loupe'><img className='icon' src="https://img.icons8.com/material-rounded/24/000000/search.png" alt='loupe'/></label>
-                <input className='search' type="search" placeholder='Search Countries...'/>
+            <div>
+                <form className='searchContainer' onSubmit={handleSubmit}>
+                    <label className='loupe'><img className='icon' src="https://img.icons8.com/material-rounded/24/000000/search.png" alt='loupe'/></label>
+                    <input className='search' type="text" value={country} placeholder='Search Countries...' onChange={handleChange}/>
+                </form>
             </div>
             <div className='filterContainer'>
                 <label className='filter'><img className='icon' src="https://img.icons8.com/material-rounded/24/000000/sorting-options.png" alt='filter'/></label>
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,4 +1,4 @@
-import { CLEAR_PAGE, FILTER_ACTIVITY, FILTER_CONTINENT, GET_ACTIVITIES, GET_COUNTRIES, GET_COUNTRIES_BY_NAME, GET_COUNTRY_DETAIL, ORDER_BY_NAME } from "../actions/actionTypes";
+import { CLEAR_PAGE, FILTER_ACTIVITY, FILTER_CONTINENT, GET_ACTIVITIES, GET_COUNTRIES, GET_COUNTRIES_BY_NAME, GET_COUNTRY_DETAIL, ORDER_BY_NAME, ORDER_BY_POPULATION } from "../actions/actionTypes";
 
 const initialState = {
     countries: [],
@@ -33,7 +33,7 @@ const rootReducer = (state = initialState, {type, payload}) => {
         case GET_COUNTRIES_BY_NAME:
             return {
                 ...state,
-                countries: payload
+                countriesCopy: payload
             }
         case FILTER_CONTINENT:
             const continentFilteredCountries = state.countries.filter(c => c.continent.includes(payload));
@@ -44,10 +44,16 @@ const rootReducer = (state = initialState, {type, payload}) => {
         case FILTER_ACTIVITY:
             
         case ORDER_BY_NAME:
-            const orderedCountries = state.countriesCopy.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
+            const orderedNameCountries = state.countriesCopy.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
             return {
                 ...state,
-                countriesCopy: payload === 'A-Z' ? orderedCountries : orderedCountries.reverse()
+                countriesCopy: payload === 'A-Z' ? orderedNameCountries : orderedNameCountries.reverse()
+            }
+        case ORDER_BY_POPULATION:
+            const orderedPopulationCountries = state.countriesCopy.sort((a, b) => a.population > b.population ? 1 : a.population < b.population ? -1 : 0);
+            return {
+                ...state,
+                countriesCopy: payload === 'Less' ? orderedPopulationCountries : orderedPopulationCountries.reverse()
             }
         default:
             return state;
