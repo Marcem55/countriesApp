84c4ebb09e3145e84a7957e6c79cb3196c9bdcf0
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2021-11-19 19:03:13 -0300

    component form, view createActivity

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { resetFilters } from '../../redux/actions';
 import './NavBar.css';
 
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    dispatch(resetFilters());
+  }
+  
     return (
         <nav className='navContainer'>
         <ul>
@@ -11,7 +19,7 @@ const NavBar = () => {
             <Link className='navLinks' to='/'><img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-earth-space-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png" alt='earth'/></Link>
           </li>
           <li>
-            <Link className='navLinks' to='/home'>HOME</Link>
+            <Link onClick={onClick} className='navLinks' to='/home'>HOME</Link>
           </li>
           <li>
             <Link className='navLinks' to='/addActivity'>CREATE ACTIVITY</Link>
--- client/src/views/CreateActivity/CreateActivity.jsx
+++ client/src/views/CreateActivity/CreateActivity.jsx
@@ -1,102 +1,13 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import Form from '../../components/Form/Form';
 import NavBar from '../../components/NavBar/NavBar';
-import constants from '../../constants';
-import { getActivities } from '../../redux/actions';
 import './CreateActivity.css';
 
 const CreateActivity = () => {
-    /* Enviar al back activity = {
-        name,
-        difficulty, (en horas)
-        duration,
-        season,
-        countries
-    } */
-    const dispatch = useDispatch();
-    const countries = useSelector(state => state.countries);
-    
-    const countriesToOrder = countries.map(country => {
-        return {
-            id: country.id,
-            name: country.name
-        }
-    });
-    const orderedCountries = countriesToOrder.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
-    // console.log(orderedCountries);
-
-    const [activity, setActivity] = useState({
-        name: '',
-        difficulty: 1,
-        duration: 1,
-        season: '',
-        countries: [] // ['Argentina', 'Brazil', 'Polonia']
-    });
-
-    const handleChange = (e) => {
-        setActivity({
-            ...activity,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const handleSelect = (e) => {
-        // Hacer un push de cada value a mi array de countries
-        const countryName = e.target.value;
-        // console.log(countryId);
-        setActivity({
-            ...activity,
-            countries: [...activity.countries, countryName]
-        });
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        await axios.post(`${constants.ACTIVITIES_URL}`, activity);
-        // console.log(activity);
-        setActivity({
-            name: '',
-            difficulty: 1,
-            duration: 1,
-            season: '',
-            countries: []
-        });
-        dispatch(getActivities());
-    }
-
     return (
         <>
             <NavBar />
-            <div className='formContainer'>
-                <h1>Add an activity!</h1>
-                <form className='form' onSubmit={handleSubmit}>
-                    <label>Name*</label>
-                    <input type="text" value={activity.name} name='name' onChange={handleChange}/>
-                    <label>Difficulty*</label>
-                    <input type="number" value={activity.difficulty} name='difficulty' onChange={handleChange} min='1' max='5'/>
-                    <label>Duration (in hours)*</label>
-                    <input type="number" value={activity.duration} name='duration' onChange={handleChange}/>
-                    <label>Season</label>
-                    <select name="season" onChange={handleChange}>
-                        <option defaultValue disabled selected>Select Season</option>
-                        <option value='Autumn'>Autumn</option>
-                        <option value='Winter'>Winter</option>
-                        <option value='Spring'>Spring</option>
-                        <option value='Summer'>Summer</option>
-                    </select>
-                    <label>Countries*</label>
-                    <select name="countries" onChange={handleSelect}>
-                        <option defaultValue disabled selected>Select Countries</option>
-                        {orderedCountries.map(country => 
-                            <option value={country.name} key={country.id}>
-                                {country.name}
-                            </option>
-                        )}
-                    </select>
-                    <button type='submit'>Add!</button>
-                </form>
-            </div>
+            <Form />
         </>
     )
 }
