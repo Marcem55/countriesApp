270b57d8225b063310927370b8115725fc6a3a42
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2021-11-29 11:04:23 -0300

    responsive landing

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -5,9 +5,9 @@ import './LandingPage.css'
 const LandingPage = () => {
   return (
     <div className='landing'>
-      <h1 className='landingTitle'>Welcome to World Tour</h1>
-      <p className='aboutText'>Find information about all countries and create tourist activities for them!</p>
-      <Link className='link' to='/home'>Go to Home</Link>
+      <h1 className='landingTitle animate__animated animate__backInDown'>Welcome to World Tour</h1>
+      <p className='aboutText animate__animated animate__backInUp'>Find information about all countries and create tourist activities for them!</p>
+      <Link className='link animate__animated animate__flipInY' to='/home'>Go to Home</Link>
     </div>
   )
 }
